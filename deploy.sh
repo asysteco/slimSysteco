00d21b0aa8139bf64d3b106f04e6388611113cdd
varo45 <alvaromelkunas9@gmail.com>
2021-09-18 11:02:30 +0200

    Add

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,7 @@
 echo "Cloning Repository"
 # Setup git
 CLONE_DIR=$(mktemp -d)
-
-git config --global user.email "alvaromelkunas9@gmail.com"
-git config --global user.name "asysteco"
-git clone --single-branch --branch "develop" "https://asysteco:$API_TOKEN_GITHUB@github.com/asysteco/slimSysteco.git" "$CLONE_DIR"
+git status
 ls -la "$CLONE_DIR"
 
 echo "Composer Install"
