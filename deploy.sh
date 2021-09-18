e0be56f2122b74dbc59c0746a0cb4912f6d141bb
varo45 <alvaromelkunas9@gmail.com>
2021-09-18 11:04:42 +0200

    Add

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,10 @@ echo "Cloning Repository"
 # Setup git
 CLONE_DIR=$(mktemp -d)
 git status
-ls -la "$CLONE_DIR"
+ls -la ./
 
 echo "Composer Install"
 
 composer install
 
-composer dump-autoload -o
-
-ls -la "$CLONE_DIR"
+ls -la ./
