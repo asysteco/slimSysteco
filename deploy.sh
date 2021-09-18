524321b2b162a04e902688dcc270546f0f7dd4ac
varo45 <alvaromelkunas9@gmail.com>
2021-09-18 11:21:43 +0200

    Add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 
 echo "Cloning Repository"
 # Setup git
+CLONE_DIR=$(mktemp -d)
 
-git checkout deploy
-git pull origin develop
-
-ls -la ./
+git config --global user.email "alvaromelkunas9@gmail.com"
+git config --global user.name "asysteco"
+git clone --single-branch --branch "deploy" "https://asysteco:$API_TOKEN_GITHUB@github.com/asysteco/slimSysteco.git" "$CLONE_DIR"
+ls -la "$CLONE_DIR"
 
 echo "Composer Install"
 
 composer install
 
-composer dump-autoload -o
+cp -r ./ "$CLONE_DIR"
+cp -ari ./.git "$CLONE_DIR"
 
-ls -la ./
-
-git add .
-git commit -m "Update deploy"
-git push
+ls -la "$CLONE_DIR"
